d4e23c72180668d72ef1369b49014004eb48ee69
Breno Henrique <brenoeevil@gmail.com>
2026-02-06 22:53:01 -0300

    feat: UserController feito!

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -7,15 +7,12 @@ class UserService {
     if (!name || !email || !password) {
       throw new Error("Nome, email e senha são obrigatórios!");
     }
-
-    const emailExists = await prisma.user.findUnique({
+    const userExists = await prisma.user.findUnique({
       where: { email },
     });
-
-    if (emailExists) {
+    if (userExists) {
       throw new Error("Usuário já existe!");
     }
-
     const user = await prisma.user.create({
       data: {
         name,
@@ -34,12 +31,56 @@ class UserService {
     if (!userId) {
       throw new Error("Id é obrigatório!");
     }
-    const user = await prisma.user.findUnique({
+    const userExists = await prisma.user.findUnique({
       where: { id: userId },
     });
-    if (!user) {
+    if (!userExists) {
       throw new Error("Usuário não encontrado!");
     }
-    return user;
+    return userExists;
+  }
+  async updateUserById(userId, data) {
+    if (!userId) {
+      throw new Error("Id é obrigatório!");
+    }
+    const userExists = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!userExists) {
+      throw new Error("Usuário não encontrado!");
+    }
+    const { name, email, password } = data;
+
+    if (email && email !== userExists.email) {
+      const emailInUse = await prisma.user.findUnique({
+        where: { email },
+      });
+      if (emailInUse) {
+        throw new Error("Email já está em uso!");
+      }
+    }
+    return await prisma.user.update({
+      where: { id: userId },
+      data: { name, email, password },
+    });
+  }
+  async deleteUserById(userId) {
+    if (!userId) {
+      throw new Error("Id é obrigatório!");
+    }
+    const userExists = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!userExists) {
+      throw new Error("Usuário não encontrado!");
+    }
+
+    await prisma.user.delete({
+      where: { id: userId },
+    });
+
+    return { message: "Usuário deletado com sucesso!" };
   }
 }
+
+export default new UserService();
